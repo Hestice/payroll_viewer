91ea919139ca67e2dd6398d8a6749c75c4495d3b
Hestice <custillano@gmail.com>
2023-08-09 10:31:33 +0800

    login screen update

--- frontend/payroll_viewer/lib/common/text_field_common.dart
+++ frontend/payroll_viewer/lib/common/text_field_common.dart
@@ -37,50 +37,38 @@ class _BuildTextFieldCommonState extends State<BuildTextFieldCommon> {
   }
 
   @override
-Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 9,
-                offset: const Offset(2, 2),
-                color: const Color(0xFFFFFFFF).withOpacity(1),
-              ),
-            ],
-          ),
-          child: TextFormField(
-            controller: widget.controller,
-            obscureText: widget.obscureText,
-            style: const TextStyle(color: Colors.black),
-            decoration: InputDecoration(
-              hintText: widget.hintText,
-              hintStyle: TextStyle(color: Colors.grey.shade500),
-              filled: true,
-              fillColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(1.0),
-                borderSide: BorderSide(color: Color.fromARGB(55, 45, 90, 138)),
-              ),
-              contentPadding: const EdgeInsets.symmetric(
-                vertical: 15,
-                horizontal: 15,
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                  color: Colors.black,
-                ),
+        TextFormField(
+          controller: widget.controller,
+          obscureText: widget.obscureText,
+          style: const TextStyle(color: Colors.black),
+          decoration: InputDecoration(
+            hintText: widget.hintText,
+            hintStyle: TextStyle(color: Colors.grey.shade500),
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(1.0),
+              borderSide: BorderSide(color: Color.fromARGB(55, 45, 90, 138)),
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 15,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: const BorderSide(
+                color: Colors.black,
               ),
             ),
-            validator: widget.validator,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            focusNode: _focusNode,
-            enabled: widget.enabled,
           ),
+          validator: widget.validator,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          focusNode: _focusNode,
+          enabled: widget.enabled,
         ),
       ],
     );
--- frontend/payroll_viewer/lib/screens/login_screen.dart
+++ frontend/payroll_viewer/lib/screens/login_screen.dart
@@ -29,163 +29,159 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Scaffold(
       backgroundColor: Colors.transparent,
       body: SafeArea(
-        child: Align(
-          alignment: Alignment.center,
-            child: Stack(
-              children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 35),
-                      Image.asset(
-                        'assets/images/ncca_logo.png',
-                        height: 150,
-                      ),
-                      const Text(
-                        'PAYROLL VIEWER',
-                        style: TextStyle(color: Color(0xFF424242), fontSize: 16),
-                      ),
-                      const SizedBox(height: 30),
-                    ],
+          child: Stack(
+            children: [
+              Positioned(
+                top: 10,
+                left: 5,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF4890D2),
+                    shape: CircleBorder(),
+                  ),
+                  onPressed: () {
+                    GoRouter.of(context).go('/');
+                    print('Back Button Clicked');
+                  },
+                  child: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                    size: 24,
                   ),
                 ),
-                Positioned(
-                  top: 10,
-                  left: 5,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF4890D2),
-                      shape: CircleBorder(),
-                    ),
-                    onPressed: () {
-                      GoRouter.of(context).go('/');
-                      print('Back Button Clicked');
-                    },
-                    child: Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                      size: 24,
-                    ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                    decoration: const BoxDecoration(
+                    color: Colors.transparent, 
                   ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                      decoration: const BoxDecoration(
-                      color: Colors.transparent, 
-                    ),
-                    child: Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 140),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                          
-                              //Text field for username/email
-                              BuildTextFieldCommon(
-                                  controller: emailController,
-                                  hintText: 'Username/Email',
-                                  obscureText: false,
-                                  validator: emailValidator,
-                                ),
-                              SizedBox(height: 16.0),
-
-                              //Text field for password 
-                              BuildTextFieldCommon(
-                                  controller: emailController,
-                                  hintText: 'Password',
-                                  obscureText: true,
-                                  validator: passwordValidator,
-                                ),
-                              SizedBox(height: 16.0),
-
-                              //forgot password
-                              GestureDetector(
-                                onTap: () {
-                                  print('Forgot Password? Clicked');
-                                },
-                                child: Align(
-                                  alignment: Alignment.centerRight,
-                                  child: Text(
-                                    'Forgot Password?',
-                                    style: TextStyle(color: Colors.grey.shade800),
+                  child: Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 140),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Align(
+                              alignment: Alignment.topCenter,
+                              child: Column(
+                                children: [
+                                  const SizedBox(height: 35),
+                                  Image.asset(
+                                    'assets/images/ncca_compact_logo.png',
+                                    height: 200,
                                   ),
-                                ),
+                                  const Text(
+                                    'PAYROLL VIEWER',
+                                    style: TextStyle(color: Color(0xFF424242), fontSize: 16),
+                                  ),
+                                  const SizedBox(height: 30),
+                                ],
                               ),
-                              SizedBox(height: 16.0),
-                              
-                              //Login button
-                              ElevatedButton(
-                                onPressed: () {
-                                  GoRouter.of(context).go('/home_screen');
-                                  // Implement login button functionality here
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                  Color.fromARGB(255, 255, 125, 81),
-                                  padding: const EdgeInsets.symmetric(vertical: 15),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(1.0),
-                                  ),
-                                  minimumSize: Size(double.infinity, 0),
-                                ),
+                            ),
+                            //Text field for username/email
+                            BuildTextFieldCommon(
+                                controller: emailController,
+                                hintText: 'Username/Email',
+                                obscureText: false,
+                                validator: emailValidator,
+                              ),
+                            SizedBox(height: 16.0),
+
+                            //Text field for password 
+                            BuildTextFieldCommon(
+                                controller: emailController,
+                                hintText: 'Password',
+                                obscureText: true,
+                                validator: passwordValidator,
+                              ),
+                            SizedBox(height: 16.0),
+
+                            //forgot password
+                            GestureDetector(
+                              onTap: () {
+                                print('Forgot Password? Clicked');
+                              },
+                              child: Align(
+                                alignment: Alignment.centerRight,
                                 child: Text(
-                                  'Login',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
+                                  'Forgot Password?',
+                                  style: TextStyle(color: Colors.grey.shade800),
                                 ),
                               ),
-                              SizedBox(height: 20),
-                            ],
-                          ),
+                            ),
+                            SizedBox(height: 16.0),
+                            
+                            //Login button
+                            ElevatedButton(
+                              onPressed: () {
+                                GoRouter.of(context).go('/home_screen');
+                                // Implement login button functionality here
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                Color.fromARGB(255, 255, 125, 81),
+                                padding: const EdgeInsets.symmetric(vertical: 15),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(1.0),
+                                ),
+                                minimumSize: Size(double.infinity, 0),
+                              ),
+                              child: Text(
+                                'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                          ],
                         ),
                       ),
                     ),
                   ),
                 ),
-                Align(
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                padding: const EdgeInsets.only(bottom: 100.0),
+                child: Text(
+                  "Don't have an account yet?",
+                  style: TextStyle(
+                    color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('Create an account? Clicked');
+                },
+                child: Align(
                   alignment: Alignment.bottomCenter,
                   child: Padding(
-                  padding: const EdgeInsets.only(bottom: 100.0),
+                  padding: const EdgeInsets.only(bottom: 80.0),
                   child: Text(
-                    "Don't have an account yet?",
+                    'Create An Account',
                     style: TextStyle(
-                      color: Colors.white,
-                      ),
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.white,
+                      color: Colors.white
                     ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    print('Create an account? Clicked');
-                  },
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Padding(
-                    padding: const EdgeInsets.only(bottom: 80.0),
-                    child: Text(
-                      'Create An Account',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        decorationColor: Colors.white,
-                        color: Colors.white
-                      ),
-                    ),
-                    ),
                   ),
                 ),
-              ]
-            ),
+              ),
+            ]
           ),
         ),
-      )
+      ),
     );
   }
 }
